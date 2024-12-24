98e152c8604c5cd7c81d40768839bcae108dd857
mazen-essam <mazenessam80@gmail.com>
2024-12-24 12:56:36 +0200

    adding more section

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./sections/Header";
 import Footer from "./sections/Footer";
 import Trips from "./sections/Trips";
+import Trips2 from "./sections/Trips2";
 function Layout() {
   return (
     <main className="font-sans">
@@ -10,6 +11,7 @@ function Layout() {
       </header>
         <section>
             <Trips />
+            <Trips2 />
         </section>
       <footer>
         <Footer />
